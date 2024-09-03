65628005da18f2c6674dbf04a5a265b75e11af62
abdoo <abdalrhmanashraf@gmail.com>
2024-09-03 06:45:51 +0300

    created add players names screen with input validation

---
+++ app/src/main/java/com/example/tic_tac_toe/AddPlayers.java
@@ -0,0 +1,44 @@
+package com.example.tic_tac_toe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_players);
+
+        final EditText playerOne = findViewById(R.id.PlayerOneName);
+        final EditText playerTwo = findViewById(R.id.PlayerTwoName);
+        final EditText startGameBtn = findViewById(R.id.StartGameBtn);
+
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String getPlayerOneName = playerOne.getText().toString();
+                final String getPlayerTwoName = playerTwo.getText().toString();
+
+                if(getPlayerOneName.isEmpty()||getPlayerTwoName.isEmpty()){
+                    Toast.makeText(AddPlayers.this, "Please enter players names", Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne",getPlayerOneName);
+                    intent.putExtra("playerTwo",getPlayerTwoName);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
